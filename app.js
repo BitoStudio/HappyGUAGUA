89e479ee76a842a233657933ee99175c7a51bbd4
weiwenchen <kxc0079@gmail.com>
2024-01-10 15:26:02 +0800

    Feature: 3 patterns

--- app.js
+++ app.js
@@ -1,36 +1,45 @@
 import { createScratchCard } from "./scratch-card.js";
 
-var canvas = document.getElementById('cover-1');
-createScratchCard(canvas, {
-    cover: 'assets/transperant.jpg',
-    brush: 'assets/brush.png',
-    brushSize: 80,
-    callbackRatio: 50,
-    callback: Mycallback1
-});
+window.onload = ()=>{
 
-createScratchCard(document.getElementById('cover-2'), {
-    cover: 'assets/silver.jpg',
-    brush: 'assets/tbr.png',
-    brushSize: 80,
-    callbackRatio: 70,
-    callback: Mycallback2
-});
+    var page = Math.floor(Math.random() * 3) + 1;
+    var pageid = 'pattern-' + page;
+    var pagttern = document.getElementById(pageid);
+    console.log(pageid);
+    pagttern.style = "display: block;";
 
-createScratchCard(document.getElementById('cover-3'), {
-    cover: 'assets/silver.jpg',
-    brush: 'assets/tbr.png',
-    brushSize: 80,
-    callbackRatio: 70,
-    callback: Mycallback3
-});
+    var canvas = document.getElementById('cover-1');
+    createScratchCard(document.getElementById('cover-' + page + '-1'), {
+        cover: 'assets/silver.jpg',
+        brush: 'assets/brush.png',
+        brushSize: 80,
+        callbackRatio: 50,
+        callback: Mycallback1
+    });
 
-function Mycallback1() {
-    console.log('my callback function 1');
-}
-function Mycallback2() {
-    console.log('my callback function 2');
-}
-function Mycallback3() {
-    console.log('my callback function 3');
+    createScratchCard(document.getElementById('cover-' + page + '-2'), {
+        cover: 'assets/silver.jpg',
+        brush: 'assets/tbr.png',
+        brushSize: 80,
+        callbackRatio: 70,
+        callback: Mycallback2
+    });
+
+    createScratchCard(document.getElementById('cover-' + page + '-3'), {
+        cover: 'assets/silver.jpg',
+        brush: 'assets/tbr.png',
+        brushSize: 80,
+        callbackRatio: 70,
+        callback: Mycallback3
+    });
+
+    function Mycallback1() {
+        console.log('my callback function 1');
+    }
+    function Mycallback2() {
+        console.log('my callback function 2');
+    }
+    function Mycallback3() {
+        console.log('my callback function 3');
+    }
 }

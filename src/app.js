4d91040656eee56a5ef9a420d1eab03e5f354a78
momentchan <mingjyunhung@gmail.com>
2024-01-12 12:54:11 +0800

    turn sound back on

--- src/app.js
+++ src/app.js
@@ -6,21 +6,20 @@ import Time from "./Util/Time.js";
 const patternNum = 3
 
 window.onload = () => {
-    const id = 1//Math.floor(Math.random() * patternNum) + 1;
+    const id = Math.floor(Math.random() * patternNum) + 1;
 
     const pattern = new Pattern(id)
     const scratchCards = new ScratchCards(id)
 
     const dragon = new Dragon()
 
-    const time = new Time()
     const sound = new Sound()
 
+    const time = new Time()
     time.on('tick', () => {
         scratchCards.update()
-        
-        // if (sound.played)
-            // sound.setVolume(scratchCards.value)
+
+        sound.setVolume(scratchCards.value)
     })
 }
 

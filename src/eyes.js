ce8a3f5003ba5fb56baa24b84abba771d843b0d1
weiwenchen <kxc0079@gmail.com>
2024-01-12 18:38:49 +0800

    update touch control

--- src/eyes.js
+++ src/eyes.js
@@ -5,13 +5,28 @@ export default class Eyes {
         this.displacement = 10
 
         this.watchRate = 0
+        this.watchDir = { x: 0, y: 0 }
+        this.touched = false
 
         this.eye = document.getElementById(`eye-container-${id}`);
         this.pupils = this.eye.querySelectorAll('.eye-pupil');
 
-        this.watchDir = 0
         window.addEventListener('mousemove', this.handleMouseMove.bind(this), false);
         window.addEventListener('touchmove', this.handleMouseMove.bind(this), false);
+
+        window.addEventListener('mousedown', this.handleMouseDown.bind(this), false);
+        window.addEventListener('touchstart', this.handleMouseDown.bind(this), false);
+
+        window.addEventListener('mouseup', this.handleMouseUp.bind(this), false);
+        window.addEventListener('touchend', this.handleMouseUp.bind(this), false);
+    }
+
+    handleMouseDown(e) {
+        this.touched = true
+    }
+
+    handleMouseUp(e) {
+        this.touched = false
     }
 
     handleMouseMove(e) {
@@ -36,10 +51,9 @@ export default class Eyes {
             const magnitude = Math.sqrt(dir.x ** 2 + dir.y ** 2);
 
             this.watchDir = {
-                x: dir.x / magnitude * this.displacement,
-                y: dir.y / magnitude * this.displacement
+                x: dir.x / magnitude * this.displacement * 0.7,
+                y: dir.y / magnitude * this.displacement * 0.7
             };
-            this.watchRate += 0.005
         })
     }
 
@@ -48,12 +62,11 @@ export default class Eyes {
     }
 
     update(elapsed) {
-        this.watchRate -= 0.01
+        this.watchRate += this.touched ? 0.05 : -0.02
         this.watchRate = Math.max(0, Math.min(this.watchRate, 1));
 
         const noiseX = noise.perlin2(elapsed * this.speed, 0.1) * this.displacement
         const noiseY = noise.perlin2(0.1, elapsed * this.speed) * this.displacement
-
         const x = this.lerp(noiseX, this.watchDir.x, this.watchRate)
         const y = this.lerp(noiseY, this.watchDir.y, this.watchRate)
 

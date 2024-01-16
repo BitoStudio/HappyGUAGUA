87548ab203bc1f53a118adf3ca0a4599e438d79b
weiwenchen <kxc0079@gmail.com>
2024-01-16 12:45:41 +0800

    adjust params

--- src/particles.js
+++ src/particles.js
@@ -5,7 +5,7 @@ var d = document, $d = $(d),
     particles = $('.particles'),
     particleCount = 0,
     sizes = [
-        15, 20, 25, 35, 45
+        5
     ],
     colors = [
         '#f44336', '#e91e63', '#9c27b0', '#673ab7', '#3f51b5',
@@ -13,6 +13,9 @@ var d = document, $d = $(d),
         '#8BC34A', '#CDDC39', '#FFEB3B', '#FFC107', '#FF9800',
         '#FF5722', '#795548', '#9E9E9E', '#607D8B', '#777777'
     ],
+
+    life = 500,
+    lifeDecay = 20,
 
     mouseX = $w.width() / 2, mouseY = $w.height() / 2;
 
@@ -28,8 +31,6 @@ $w
 
 $d
     .on('mousemove touchmove', function (event) {
-        event.preventDefault();
-        event.stopPropagation();
         mouseX = event.clientX;
         mouseY = event.clientY;
         if (!!event.originalEvent.touches) {
@@ -47,15 +48,14 @@ function createParticle(event) {
         color = colors[Math.floor(Math.random() * colors.length)],
         negative = size / 2,
         speedHorz = Math.random() * 10,
-        speedUp = Math.random() * 25,
+        speedUp = 0,//Math.random() * 25,
         spinVal = 360 * Math.random(),
         spinSpeed = ((36 * Math.random())) * (Math.random() <= .5 ? -1 : 1),
         otime,
-        time = otime = (1 + (.5 * Math.random())) * 1000,
+        time = otime = (1 + (.5 * Math.random())) * life,
         top = (mouseY - negative),
         left = (mouseX - negative),
-        direction = Math.random() <= .5 ? -1 : 1,
-        life = 10;
+        direction = Math.random() <= .5 ? -1 : 1;
 
     particle
         .css({
@@ -72,10 +72,10 @@ function createParticle(event) {
     updateParticleCount();
 
     var particleTimer = setInterval(function () {
-        time = time - life;
-        left = left - (speedHorz * direction);
+        time = time - lifeDecay;
+        // left = left - (speedHorz * direction);
         top = top - speedUp;
-        speedUp = Math.min(size, speedUp - 1);
+        speedUp = Math.min(size, speedUp - 0.1);
         spinVal = spinVal + spinSpeed;
 
 

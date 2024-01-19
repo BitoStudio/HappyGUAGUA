b820d04c6542ca19c368f8467aa23eb559ab6dc5
weiwenchen <kxc0079@gmail.com>
2024-01-19 11:36:34 +0800

    implement loading page

--- src/app.js
+++ src/app.js
@@ -6,24 +6,53 @@ import Time from "./Util/Time.js";
 import source from "./source.js";
 
 window.onload = () => {
-    const id = Math.floor(Math.random() * 3) + 1;
-    const patternData = source.patterns[id - 1]
 
-    const pattern = new Pattern(id, patternData)
-    const scratchCardHolder = new ScratchHolder(id, source.scratchs)
-    const dragon = new Dragon(id, patternData.dragon)
+    // quick debug flag
+    const useLoadingPage = true
 
-    // const sound = new Sound()
+    // Loading Page
+    {
+        const loadButton = document.getElementById('loadButton');
+        const loadingPage = document.getElementById('loading-page');
+        const mainPage = document.getElementById('main-page');
 
-    const time = new Time()
+        if (useLoadingPage) {
+            loadButton.addEventListener('click', function () {
+                loadButton.disabled = true;
 
-    time.on('tick', () => {
-
-        if (scratchCardHolder != null) {
-            scratchCardHolder.update()
-            // sound.setVolume(scratchCards.value)
+                setTimeout(function () {
+                    loadingPage.classList.add('hidden');
+                    mainPage.classList.remove('hidden');
+                }, 2000);
+            });
+        } else {
+            loadingPage.classList.add('hidden');
+            mainPage.classList.remove('hidden');
         }
-        if (dragon.ready)
-            dragon.eyes.update(time.elapsed)
-    })
+    }
+
+
+    // Main Page
+    {
+        const id = Math.floor(Math.random() * 3) + 1;
+        const patternData = source.patterns[id - 1]
+
+        const pattern = new Pattern(id, patternData)
+        const scratchHolder = new ScratchHolder(id, source.scratchs)
+        const dragon = new Dragon(id, patternData.dragon)
+
+        const sound = new Sound()
+
+        const time = new Time()
+
+        time.on('tick', () => {
+
+            if (scratchHolder != null) {
+                scratchHolder.update()
+                sound.setVolume(scratchHolder.value)
+            }
+            if (dragon.ready)
+                dragon.eyes.update(time.elapsed)
+        })
+    }
 }
